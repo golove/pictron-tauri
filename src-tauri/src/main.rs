898c240361f93f58281f50103873aa024d9976e2
golove <liuziyu7@gmail.com>
2024-02-04 16:50:10 +0800

    add action yml

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
 #[tauri::command]
 fn greet(name: &str) -> String {
    format!("Hello, {}!", name)
-}
-
-#[tauri::command]
-fn showName(name: &str) -> String {
-  format!("what's up, {}!", name)
 }
 
 #[tauri::command]
@@ -17,11 +13,14 @@ format!("spider successful")
 }
 
 
-
-
 fn main() {
+  // 目标网页的URL
+  // let url = "http://dkleh8.xyz/pw/thread.php?fid=14";
+
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet,showName,spider])
+    .invoke_handler(tauri::generate_handler![greet,spider])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
+  
 }
